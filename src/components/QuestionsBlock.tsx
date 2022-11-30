b73136ddd02ceca5ca0b98674cff287e0bbc63b5
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-30 12:00:18 +0100

    questions block tsx and handle click for the answers

--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,13 +1,35 @@
 import { Content, Question } from '../../interfaces'
 import QuestionBlock from './QuestionBlock'
 
-const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
+const QuestionsBlock = ({
+  quizItem,
+  chosenAnsweritems,
+  setChosenAnsweritems,
+  unansweredQuestionIds,
+  setUnansweredQuestionIds,
+}: {
+  quizItem: Content
+  chosenAnsweritems: string[]
+  setChosenAnsweritems: Function
+  unansweredQuestionIds: number[] | undefined
+  setUnansweredQuestionIds: Function
+}) => {
   return (
     <>
-      <h2 className="title__block" id={String(quizItem.id)}>{quizItem.text}</h2>
+      <h2 className="title__block" id={String(quizItem.id)}>
+        {quizItem.text}
+      </h2>
       <div className="questions__container">
         {quizItem.questions.map((question: Question, _index) => (
-          <QuestionBlock question={question} key={_index} />
+          <QuestionBlock
+            question={question}
+            quizItemId={quizItem.id}
+            key={_index}
+            chosenAnsweritems={chosenAnsweritems}
+            setChosenAnsweritems={setChosenAnsweritems}
+            unansweredQuestionIds={unansweredQuestionIds}
+            setUnansweredQuestionIds={setUnansweredQuestionIds}
+          />
         ))}
       </div>
     </>
