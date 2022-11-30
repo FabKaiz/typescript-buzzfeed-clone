2dd9e6be1b9455eb82ac6ef0325ef4cac70ccb6e
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-30 13:18:15 +0100

    started answerblock tsx

--- src/components/AnswerBlock.tsx
+++ src/components/AnswerBlock.tsx
@@ -1,6 +1,33 @@
-const AnswerBlock = () => {
+import { useEffect, useState } from "react"
+import { Answer } from "../../interfaces"
+
+const AnswerBlock = ({answerOptions, chosenAnswerItems} : {
+  answerOptions: Answer[] | undefined
+  chosenAnswerItems: string[]
+}) => {
+
+  const [result, setResult] = useState<Answer | null>()
+
+  useEffect(() => {
+    answerOptions?.forEach((answer: Answer) => {
+      if (
+        chosenAnswerItems.includes(answer.combination[0]) &&
+        chosenAnswerItems.includes(answer.combination[1]) &&
+        chosenAnswerItems.includes(answer.combination[2])
+        ) {
+          setResult(answer)
+        }
+    })
+  }, [chosenAnswerItems, answerOptions])
+
+  console.log(result);
+  
+  
   return (
-    <div>AnswerBlock</div>
+    <div id="answer__block" className="answer__block">
+      <h2>{result?.text}</h2>
+      <img src={result?.image} alt={result?.alt} />
+    </div>
   )
 }
 
