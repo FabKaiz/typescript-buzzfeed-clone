f1b5b56afe257874e9ac08f3bbb28479cb8b0ce2
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-30 11:11:55 +0100

    fetched data from server and mapped over the quesitons block

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import Title from "./components/Title";
+import { useEffect, useState } from 'react'
+import Title from './components/Title'
+import { Content, QuizData } from '../interfaces'
+import QuestionsBlock from './components/QuestionsBlock'
 
 const App = () => {
+  const [quiz, setQuiz] = useState<QuizData | null>()
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/quiz-item')
+      const json = await response.json()
+      setQuiz(json)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(quiz)
+
   return (
     <div className="App">
-      <Title />
+      <Title title={quiz?.title} subtitle={quiz?.subtitle} />
+      {quiz?.content.map((content: Content, id: Content['id']) => (
+        <QuestionsBlock quizItem={content} key={id} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
