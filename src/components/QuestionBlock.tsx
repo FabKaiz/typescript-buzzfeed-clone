1409d97852d9331eaa456565f3e2a1f1693ae3bb
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-30 12:00:11 +0100

    questions block tsx and handle click for the answers

--- src/components/QuestionBlock.tsx
+++ src/components/QuestionBlock.tsx
@@ -1,6 +1,52 @@
-const QuestionBlock = () => {
+import { Question } from '../../interfaces'
+
+const QuestionBlock = ({
+  question,
+  quizItemId,
+  chosenAnsweritems,
+  setChosenAnsweritems,
+  unansweredQuestionIds,
+  setUnansweredQuestionIds,
+}: {
+  question: Question
+  quizItemId: number
+  chosenAnsweritems: string[]
+  setChosenAnsweritems: Function
+  unansweredQuestionIds: number[] | undefined
+  setUnansweredQuestionIds: Function
+}) => {
+  const handleClick = () => {
+    if (!chosenAnsweritems?.includes(question.text)) {
+      setChosenAnsweritems((prev: string[]) => [...prev, question.text])
+      setUnansweredQuestionIds(
+        unansweredQuestionIds?.filter((id: number) => id !== quizItemId)
+      )
+    } else {
+      setChosenAnsweritems(
+        chosenAnsweritems?.filter((item: string) => item !== question.text)
+      )
+      setUnansweredQuestionIds((prev: number[]) => [...prev, quizItemId])
+    }
+  }
+
+  const validPick =
+    !chosenAnsweritems?.includes(question.text) &&
+    !unansweredQuestionIds?.includes(quizItemId)
+
   return (
-    <div>QuestionBlock</div>
+    <button
+      className="question__block"
+      onClick={handleClick}
+      disabled={validPick}
+    >
+      <img src={question?.image} alt={question?.alt} />
+      <h3>{question?.text}</h3>
+      <p>
+        <a href={question?.image} target="_blank" rel="noreferrer">
+          {question?.credit}
+        </a>
+      </p>
+    </button>
   )
 }
 
