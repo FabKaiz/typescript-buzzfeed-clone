28fab0a5b92eeb538ba67a7bc40023271b5d21fa
Fabien Chareun <cacrfabfr@gmail.com>
2022-11-30 11:11:16 +0100

    Questions block tsx

--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,6 +1,16 @@
-const QuestionsBlock = () => {
+import { Content, Question } from '../../interfaces'
+import QuestionBlock from './QuestionBlock'
+
+const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
   return (
-    <div>QuestionsBlock</div>
+    <>
+      <h2 className="title__block" id={String(quizItem.id)}>{quizItem.text}</h2>
+      <div className="questions__container">
+        {quizItem.questions.map((question: Question, _index) => (
+          <QuestionBlock question={question} key={_index} />
+        ))}
+      </div>
+    </>
   )
 }
 
